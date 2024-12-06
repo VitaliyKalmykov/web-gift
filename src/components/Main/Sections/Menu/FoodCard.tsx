a86e440b52a3ef4cf7d4c51a9ca79b7af45c041b
Vitalii <vitaliy.kalmykov.work@gmail.com>
2024-12-06 15:07:35 +0200

    add modal and remove sidebar

--- src/components/Main/Sections/Menu/FoodCard.tsx
+++ src/components/Main/Sections/Menu/FoodCard.tsx
@@ -3,19 +3,26 @@ interface IFoodCard {
     name: string;
     description: string;
     price: number;
+    onButtonClick: () => void;
 }
 
 
-const FoodCard = ({image, name, description, price}: IFoodCard) => {
+const FoodCard = ({image, name, description, price, onButtonClick}: IFoodCard) => {
     return (
         <article className="border-2 p-2 rounded-xl shadow-xl">
+
             <ul className="flex flex-col justify-center items-center gap-2">
                 <li className="flex justify-center items-center"><img className="w-48" src={image} alt={name}/></li>
                 <li><h3>{name}</h3></li>
                 <li>{description}</li>
-                <li className="mb-2">{price} грн.</li>
+                <li className="mb-2">{price} грн./100 грам</li>
             </ul>
-            <button type={"button"} className="block mx-auto border-2 p-2 rounded-lg">До столу</button>
+
+            <button type={"button"}
+                    className="block mx-auto border-2 p-2 rounded-lg"
+                    onClick={onButtonClick}>
+                До столу
+            </button>
         </article>
     );
 };
--- src/components/Main/Sections/Menu/Menu.tsx
+++ src/components/Main/Sections/Menu/Menu.tsx
@@ -1,3 +1,4 @@
+import  {useState, useEffect} from "react";
 import Jerk1 from '../../../../images/jerks1.webp'
 import Jerk2 from '../../../../images/jerks2.jpg'
 import Jerk3 from '../../../../images/jerks3.webp'
@@ -5,41 +6,73 @@ import FoodCard from "./FoodCard";
 
 const Menu = () => {
 
-    const jerksArray = [{
+    // Інтерфейс для об'єктів
+    interface IJerk {
+        image: string; // Шлях до зображення
+        name: string; // Назва джерки
+        description: string; // Опис джерки
+        price: number; // Ціна джерки
+    }
+
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [selectedJerk, setSelectedJerk] = useState<IJerk | null>(null);
+
+    //обробник обранної джерки для модалки
+    const handleButtonClick = (jerk: IJerk) => {
+        setSelectedJerk(jerk);
+        setIsModalOpen(true);
+    };
+
+    //закриваєм модалку та скидуємо стан обраної джерки
+    const closeModal = () => {
+        setIsModalOpen(false);
+        setSelectedJerk(null)
+    };
+
+    //носкролл якщо модалка відкрита
+    useEffect(() => {
+        if (isModalOpen) {
+            document.body.style.overflow = "hidden"; // При відкритті модалки прибираємо скролл
+        } else {
+            document.body.style.overflow = "auto"; // При закритті модалки повертаємо скролл
+        }
+    }, [isModalOpen]);
+
+    const jerksArray: IJerk[] = [{
         image: Jerk1,
-        name: 'Назва джерок',
+        name: 'Джерка1',
         description: 'Ці джерки підкреслюють вкус (вкус джерок) котрий ідеально пасує з (додати щось). Ідеальний вибір для (додати щось)',
         price: 100
     },
         {
             image: Jerk2,
-            name: 'Назва джерок',
+            name: 'Джерка2',
             description: 'Ці джерки підкреслюють вкус (вкус джерок) котрий ідеально пасує з (додати щось). Ідеальний вибір для (додати щось)',
             price: 150
         },
         {
             image: Jerk3,
-            name: 'Назва джерок',
+            name: 'Джерка3',
             description: 'Ці джерки підкреслюють вкус (вкус джерок) котрий ідеально пасує з (додати щось). Ідеальний вибір для (додати щось)',
             price: 200
         },
         {
             image: Jerk3,
-            name: 'Назва джерок',
+            name: 'Джерка4',
             description: 'Ці джерки підкреслюють вкус (вкус джерок) котрий ідеально пасує з (додати щось). Ідеальний вибір для (додати щось)',
             price: 250
         }
 ,
         {
             image: Jerk3,
-            name: 'Назва джерок',
+            name: 'Джерка5',
             description: 'Ці джерки підкреслюють вкус (вкус джерок) котрий ідеально пасує з (додати щось). Ідеальний вибір для (додати щось)',
             price: 300
         }
 ,
         {
             image: Jerk3,
-            name: 'Назва джерок',
+            name: 'Джерка6',
             description: 'Ці джерки підкреслюють вкус (вкус джерок) котрий ідеально пасує з (додати щось). Ідеальний вибір для (додати щось)',
             price: 350
         }
@@ -48,20 +81,43 @@ const Menu = () => {
     return (
         <section>
             <div className="container flex justify-between gap-2 relative">
-                <div className="border-2 p-2"><p>text sidebar some filter</p>
-                    <p>text sidebar some filter</p>
-                    <p>text sidebar some filter</p>
-                    <p>text sidebar some filter</p>
-                    <p>text sidebar some filter</p>
-                    <p>text sidebar some filter</p>
-                    <p>text sidebar some filter</p></div>
                 <div className="flex-1 border-2">
                     <div className="grid grid-cols-3 gap-5">
                         {jerksArray.map(((jerk, index) => (
-                            <FoodCard key={index} image={jerk.image} name={jerk.name} description={jerk.description} price={jerk.price} />
+                            <FoodCard key={index}
+                                      image={jerk.image}
+                                      name={jerk.name}
+                                      description={jerk.description}
+                                      price={jerk.price}
+                                      onButtonClick={() => handleButtonClick(jerk)}
+                            />
                         )))}
                     </div>
                 </div>
+
+
+
+                {/*модалка*/}
+                {isModalOpen && (
+                    <div
+                        className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50"
+                         onClick={closeModal}
+                    >
+                        <div className="bg-white p-5 rounded-xl shadow-lg" onClick={(e )=> e.stopPropagation()}>
+                            <h2 className="text-lg font-bold">{selectedJerk?.name}</h2>
+                            <p>{selectedJerk?.description}</p>
+                            <p className="mt-2 font-bold">{selectedJerk?.price} грн.</p>
+                            <button
+                                className="mt-4 p-2 bg-red-500 text-white rounded-lg"
+                                onClick={closeModal}
+                            >
+                                Закрити
+                            </button>
+                        </div>
+                    </div>
+                )}
+
+
             </div>
         </section>
     );
