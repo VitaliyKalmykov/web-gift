23e62fa6f1d093deda3637a465378b176bd6dd34
Vitalii <vitaliy.kalmykov.work@gmail.com>
2024-12-14 18:50:56 +0200

    add formspree, validation and submit call modal form

--- src/components/CallModal/CallModal.tsx
+++ src/components/CallModal/CallModal.tsx
@@ -1,49 +1,83 @@
-import { useContext } from "react";
+import { useContext, useRef } from "react";
 import { CallContext } from "../../callContext/CallContext";
 import Button from "../UI/Button";
 import CallModalForm from "./CallModalForm";
 
 const CallModal = () => {
+
     const callContext = useContext(CallContext); // Отримуємо контекст
 
-    if (!callContext) {
-        // Якщо контекст не ініціалізований, не рендеримо компонент
-        return null;
-    }
+    // Навіть якщо `callContext` не існує, хуки все одно викликаються
+    const { isCallVisible, setIsCallVisible } = callContext || {};
 
-    const { isCallVisible, setIsCallVisible } = callContext;
+    //useref для поєднання кнопок та форми
+    const formRef = useRef<HTMLFormElement>(null);
 
-    if (!isCallVisible) {
-        // Якщо модалка має бути прихованою, не рендеримо компонент
-        return null;
+    const focusOnForm = () => {
+        if(formRef.current) {
+            formRef.current.scrollIntoView({behavior: "smooth"});
+        }
     }
 
     const closeModal = () => {
-        setIsCallVisible(false);
+        if(setIsCallVisible) {
+            setIsCallVisible(false);
+        }
     }
 
 
-       return (
-           <div onClick={closeModal} className="p-4 fixed inset-0 bg-black bg-opacity-70 flex justify-center items-center z-50">
-               <div onClick={(e) => {e.stopPropagation()}} className="bg-white rounded-lg shadow-lg p-6">
-                  <h2 className="text-center mb-4">Маєте пропозицію чи хочете оформити замовленя по телефону?</h2>
-                   <h3>Заповніть форму та я особисто зв&apos;яжусь з вами найближчим часом:</h3>
-                   <CallModalForm/>
-                  <div className="flex flex-1 justify-between">
-                      <Button
-                          className="mt-4 px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600"
-                      >
-                          Замовити дзвінок
-                      </Button>
-                      <Button
-                          className="mt-4 px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600"
-                          onClick={closeModal}
-                      >
-                          Close
-                      </Button>
-                  </div>
-               </div>
-           </div>
+    // Якщо контекст не ініціалізований, не рендеримо компонент
+    if (!callContext) {
+        return null;
+    }
+
+    // Якщо модалка має бути прихованою, не рендеримо компонент
+    if (!isCallVisible) {
+        return null;
+    }
+
+    return (
+           <>
+               {isCallVisible && (
+                   <div
+                       onClick={closeModal}
+                       className="p-4 fixed inset-0 bg-black bg-opacity-70 flex justify-center items-center z-50"
+                   >
+                       <div
+                           onClick={(e) => e.stopPropagation()}
+                           className="bg-white rounded-lg shadow-lg p-6"
+                       >
+                           <h2 className="text-center mb-4">
+                               Маєте пропозицію чи хочете оформити замовлення по телефону?
+                           </h2>
+                           <h3>
+                               Заповніть форму, і я особисто зв&apos;яжусь з вами найближчим часом:
+                           </h3>
+                           <CallModalForm formRef={formRef} />
+                           <div className="flex flex-1 justify-between">
+                               <Button
+                                   type="button"
+                                   onClick={() => {
+                                       focusOnForm();
+                                       if(formRef.current) {
+                                           formRef.current.requestSubmit();
+                                       }
+                                   }}
+                                   className="mt-4 px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600"
+                               >
+                                   Замовити дзвінок
+                               </Button>
+                               <Button
+                                   className="mt-4 px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600"
+                                   onClick={closeModal}
+                               >
+                                   Close
+                               </Button>
+                           </div>
+                       </div>
+                   </div>
+               )}
+           </>
        );
 };
 
--- src/components/CallModal/CallModalForm.tsx
+++ src/components/CallModal/CallModalForm.tsx
@@ -1,23 +1,82 @@
-import {useState} from "react";
+import React, {useState} from "react";
 import Input from "../UI/Input";
+import { useForm } from '@formspree/react';
 
-const CallModalForm = () => {
+interface ICallModalProps {
+ formRef: React.RefObject<HTMLFormElement>;
+}
 
-    //інпут приймає тількі цифри
-    const handleInputChange = (event:any):void => {
-        event.target.value = event.target.value.replace(/[^0-9+]/g, '');
-    }
+
+const CallModalForm = ({formRef}: ICallModalProps) => {
 
     //textarea на 240 символів
     const [text, setText] = useState("");
 
-    // Обробник зміни тексту
+    // Обробник зміни тексту textarea
     const handleChange = (e:any):void => {
         setText(e.target.value);
     };
 
+    //умовни стилі
+    const counterStyle = text.length === 240 ? "text-red-500" : "text-gray-500";
+
+    //валідація форми
+    const [name, setName] = useState("");
+    const [phoneNumber, setPhoneNumber] = useState("");
+    const [errors, setErrors] = useState<{ name?: string; phoneNumber?: string }>({});
+
+    //Інпут номеру телефону
+    //інпут приймає тількі цифри
+    const handleInputPhoneChange = (event:any):void => {
+        event.target.value = event.target.value.replace(/[^0-9+]/g, '');
+        setPhoneNumber(event.target.value);
+    }
+
+    //Інпут нейму
+    const handleInputNameChange = (event:any):void => {
+        setName(event.target.value);
+    }
+
+
+    const validateForm = () => {
+        const newErrors: { name?: string; phoneNumber?: string } = {};
+        if (!name.trim()) {
+            newErrors.name = "Будь ласка вкажіть як до вас звертатись";
+        }
+        if (!/^(\+380|0)\d{9}$/.test(phoneNumber)) {
+            newErrors.phoneNumber = "Телефон повинен бути у форматі +380XXXXXXXXX.";
+        }
+        setErrors(newErrors);
+        return Object.keys(newErrors).length === 0;
+    };
+
+
+    //formspree + валідація форми
+
+    const [state, handleSubmit] = useForm("xeoqwynq");
+
+
+    const formSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        if (validateForm()) {
+            // Якщо валідація успішна, відправляємо форму через Formspree
+            handleSubmit(event);
+        } else {
+            console.log("форма містить помилки");
+        }
+    }
+
+
+
     return (
-        <form className="p-4 flex flex-col">
+        <>
+        {state.succeeded  && <p className="text-green-900 font-bold text-xl">Дякую за замовлення ! З вами скоро зв&apos;яжуться!</p>}
+        <form
+            ref={formRef}
+            onSubmit={formSubmit}
+            action="https://formspree.io/f/xeoqwynq"
+            method="POST"
+            className="p-4 flex flex-col">
             <Input
                 htmlFor={"name"}
                    type={"text"}
@@ -25,7 +84,10 @@ const CallModalForm = () => {
                    name={"Name"}
                    placeholder={"Ваше ім'я..."}
                    classNameInput={"w-full border-2 border-blue-100 rounded-full p-4 mb-4"}
-            />
+                onChange={handleInputNameChange}
+            >
+                {errors.name && <p className="text-red-500 text-sm mb-2">{errors.name}</p>}
+            </Input>
             <Input
                 htmlFor={"phoneNumber"}
                 type={"text"}
@@ -33,21 +95,25 @@ const CallModalForm = () => {
                 name={"Phone Number"}
                 placeholder={"Номер телефону: +380..."}
                 classNameInput={"w-full border-2 border-blue-100 rounded-full p-4 mb-4"}
-                onInput={handleInputChange}
-            />
+                onInput={handleInputPhoneChange}
+            >
+                {errors.phoneNumber && <p className="text-red-500 text-sm mb-2">{errors.phoneNumber}</p>}
+            </Input>
             <div className="relative">
             <textarea
                 maxLength={240}
                 placeholder={"Ваше зверненя..."}
                 className="w-full h-48 border-2 border-blue-100 rounded-xl resize-none p-4 mb-4"
                 value={text}
+                name="Message"
                 onChange={handleChange}
             />
-                <div className="absolute bottom-7 right-4 text-sm text-gray-500">
+                <div className={`absolute bottom-7 right-4 text-sm ${counterStyle}`}>
                     {text.length} / 240
                 </div>
             </div>
         </form>
+        </>
     )
 }
 
